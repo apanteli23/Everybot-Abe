57f1519da06bdb7ac57d82c21c57b3f9eac255cd
Programming 2 <apanteli23@team3467.org>
2021-11-04 20:40:55 -0400

    Added driving Command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,11 +5,13 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
-
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.control.XboxController;
+import frc.robot.control.XboxControllerButton;
 import frc.robot.subsystems.ClimberSubsystem.Climb;
 import frc.robot.subsystems.ClimberSubsystem.ClimberSubsystem;
+import frc.robot.subsystems.DrivebaseSubsystem.ArcadeDrive;
+import frc.robot.subsystems.DrivebaseSubsystem.DriveSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,9 +21,11 @@ import frc.robot.subsystems.ClimberSubsystem.ClimberSubsystem;
  */
 public class RobotContainer {
 
-  private final ClimberSubsystem climber = new ClimberSubsystem();
+  private final ClimberSubsystem m_climber = new ClimberSubsystem();
+  private final DriveSubsystem m_drive = new DriveSubsystem();
 
-  public static XboxController controller = new XboxController(0);
+  public static XboxController m_driverController = new XboxController(0);
+  public static XboxController m_operatorController = new XboxController(1);
 
   // The robot's subsystems and commands are defined here...
  
@@ -30,7 +34,12 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
-  }
+
+    m_drive.setDefaultCommand(
+        new ArcadeDrive(m_drive, 
+                        () -> m_driverController.getLeftY(), 
+                        () -> m_driverController.getRightX()));
+    }
 
   /**
    * Use this method to define your button->command mappings. Buttons can be created by
@@ -39,13 +48,14 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    if(controller.getAButton()){
-      new Climb(climber, 0.5);
-    }
     
-    if(controller.getBButton()){
-      new Climb(climber, -0.5);
-    }
+
+    new XboxControllerButton(m_operatorController, XboxController.Button.kA)
+        .whenHeld(new Climb(m_climber, 0.5));
+
+    new XboxControllerButton(m_operatorController, XboxController.Button.kY)
+        .whenHeld(new Climb(m_climber, -0.5));
+
   }
 
   /**
--- src/main/java/frc/robot/subsystems/DrivebaseSubsystem/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivebaseSubsystem/DriveSubsystem.java
@@ -5,22 +5,25 @@
 package frc.robot.subsystems.DrivebaseSubsystem;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DriveSubsystem extends SubsystemBase {
   /** Creates a new DriveSubsystem. */
  
-  TalonSRX m_RightDrive1 = new TalonSRX(Constants.CanConstants.kdriveMotorRight1);
-  TalonSRX m_RightDrive2 = new TalonSRX(Constants.CanConstants.kdriveMotorRight2);
-  TalonSRX m_leftDrive1 = new TalonSRX(Constants.CanConstants.kdriveMotorLeft1);
-  TalonSRX m_leftDrive2 = new TalonSRX(Constants.CanConstants.kdriveMotorLeft2);
+  private final WPI_TalonSRX m_rightDrive1 = new WPI_TalonSRX(Constants.CanConstants.kdriveMotorRight1);
+  private final WPI_TalonSRX m_rightDrive2 = new WPI_TalonSRX(Constants.CanConstants.kdriveMotorRight2);
+  private final WPI_TalonSRX m_leftDrive1 = new WPI_TalonSRX(Constants.CanConstants.kdriveMotorLeft1);
+  private final WPI_TalonSRX m_leftDrive2 = new WPI_TalonSRX(Constants.CanConstants.kdriveMotorLeft2);
+
+  private final DifferentialDrive m_drive = new DifferentialDrive(m_rightDrive1, m_leftDrive1);
 
   public DriveSubsystem() {
-    m_RightDrive1.configFactoryDefault();
-    m_RightDrive2.configFactoryDefault();
+    m_rightDrive1.configFactoryDefault();
+    m_rightDrive2.configFactoryDefault();
     m_leftDrive1.configFactoryDefault();
     m_leftDrive2.configFactoryDefault();
 
@@ -28,7 +31,8 @@ public class DriveSubsystem extends SubsystemBase {
     m_leftDrive2.setInverted(true);
 
     m_leftDrive2.follow(m_leftDrive1);
-    m_RightDrive2.follow(m_RightDrive1);
+    m_rightDrive2.follow(m_rightDrive1);
+
 
   }
 
@@ -38,11 +42,16 @@ public class DriveSubsystem extends SubsystemBase {
   }
 
   public void driveRight(double speed){
-    m_RightDrive1.set(ControlMode.PercentOutput, speed);
+    m_rightDrive1.set(ControlMode.PercentOutput, speed);
   }
 
   public void driveLeft(double speed){
     m_leftDrive1.set(ControlMode.PercentOutput, speed);
+  }
+
+  public void arcadeDrive(double fwd, double rot)
+  { 
+    m_drive.arcadeDrive(fwd, rot);
   }
 
 
